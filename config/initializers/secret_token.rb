ffd8bffbf29e53947e5d530dfe07b581a10e17e7
marco-lindsay <lindsay_marco@hotmail.com>
2014-02-12 14:37:11 -0800

    baseline for new project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TwitClone::Application.config.secret_key_base = 'f3e045b6b132ee685a6d6c82e35d0ce182f4beef035087459a639047bd507831535202d15b14b79294074a8fff3ad6c94b3427653dac1ba3acad9e6b6f6110fe'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+TwitClone::Application.config.session_store :cookie_store, key: '_twit_clone_session'
