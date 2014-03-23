7adf9f598bd430ec1be9273955c2b6188ae80bc0
marco-lindsay <lindsay_marco@hotmail.com>
2014-03-23 15:18:57 -0700

    navbar and prep for socialization

--- app/models/relationship.rb
+++ app/models/relationship.rb
@@ -1,6 +1,6 @@
 class Relationship < ActiveRecord::Base
 
-	belongs_to :watcher, class_name: "User"
-	belongs_to :watched, class_name: "User"
+	# belongs_to :watcher, class_name: "User"
+	# belongs_to :watched, class_name: "User"
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,11 +3,11 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-	has_many :posts
-	has_many :watcher_relationships, class_name: "Relationship", foreign_key: "watched_id"
-	has_many :watched_relationships, class_name: "Relationship", foreign_key: "watcher_id"
-	has_many :watchers, through: :watcher_relationships
-	has_many :watcheds, through: :watched_relationships
+	has_many :posts,  dependent: :destroy
+	# has_many :watcher_relationships, class_name: "Relationship", foreign_key: "watched_id"
+	# has_many :watched_relationships, class_name: "Relationship", foreign_key: "watcher_id"
+	# has_many :watchers, through: :watcher_relationships
+	# has_many :watcheds, through: :watched_relationships
 	
 
 	validates_presence_of :name, :email
